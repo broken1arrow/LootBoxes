3d03e2caa6871751d0a6e164dccaac0d3e224a88
broken1arrow <broken2arrow1@gmail.com>
2022-03-18 11:19:02 +0100

    added check if placeholder api is installed.

--- src/main/java/org/brokenarrow/lootboxes/Lootboxes.java
+++ src/main/java/org/brokenarrow/lootboxes/Lootboxes.java
@@ -5,12 +5,14 @@ import org.brokenarrow.lootboxes.commands.CommandsGroup;
 import org.brokenarrow.lootboxes.lootdata.ItemData;
 import org.brokenarrow.lootboxes.lootdata.LootItems;
 import org.brokenarrow.lootboxes.runTask.RunTask;
+import org.brokenarrow.lootboxes.settings.GuiTempletSettings;
 import org.brokenarrow.lootboxes.settings.Settings;
 import org.brokenarrow.lootboxes.tasks.SpawnLootContainer;
 import org.brokenarrow.lootboxes.untlity.command.CommandGroupUtility;
 import org.brokenarrow.lootboxes.untlity.command.CommandGroupUtilityAPI;
 import org.brokenarrow.lootboxes.untlity.command.CommandRegister;
 import org.brokenarrow.menu.library.RegisterMenuAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
@@ -22,6 +24,7 @@ public class Lootboxes extends JavaPlugin {
 	private Settings settings;
 	private SpawnLootContainer spawnLootContainer;
 
+	private boolean placeholderAPIMissing;
 	private CommandGroupUtility commandGroupUtility;
 	private CommandRegister commandRegister;
 	private CommandsGroup commandsGroup;
@@ -33,13 +36,24 @@ public class Lootboxes extends JavaPlugin {
 		this.runTask = new RunTask(this);
 		this.runTask.start();
 		this.settings = new Settings();
+		GuiTempletSettings.getInstance().reload();
+
 		this.settings.reload();
 		LootItems.getInstance().reload();
 		ItemData.getInstance().reload();
 		new RegisterMenuAPI(this);
 
 		commandRegister = new CommandRegister(this, "lootbox", new CommandsGroup());
-
+		if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+			/*
+			 * We register the EventListener here, when PlaceholderAPI is installed.
+			 * Since all events are in the main class (this class), we simply use "this"
+			 */
+			placeholderAPIMissing = false;
+			//Bukkit.getPluginManager().registerEvents(this, this);
+		} else {
+			placeholderAPIMissing = true;
+		}
 		this.getLogger().log(Level.INFO, "Start Lootboxes");
 	}
 
@@ -65,5 +79,9 @@ public class Lootboxes extends JavaPlugin {
 
 	public SpawnLootContainer getSpawnLootContainer() {
 		return spawnLootContainer;
+	}
+
+	public boolean isPlaceholderAPIMissing() {
+		return placeholderAPIMissing;
 	}
 }
