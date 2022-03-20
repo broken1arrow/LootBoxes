5bf36d868add77029b38b954785b4cebc7ceae0f
broken1arrow <broken2arrow1@gmail.com>
2022-03-20 14:50:04 +0100

    added arrows so you can change page.

--- src/main/java/org/brokenarrow/lootboxes/menus/EditCreateLootTable.java
+++ src/main/java/org/brokenarrow/lootboxes/menus/EditCreateLootTable.java
@@ -6,6 +6,7 @@ import org.brokenarrow.lootboxes.lootdata.LootItems;
 import org.brokenarrow.lootboxes.untlity.CreateItemUtily;
 import org.brokenarrow.menu.library.MenuButton;
 import org.brokenarrow.menu.library.MenuHolder;
+import org.brokenarrow.menu.library.NMS.UpdateTittleContainers;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
@@ -24,12 +25,14 @@ public class EditCreateLootTable extends MenuHolder {
 	private final MenuButton backButton;
 	private final MenuButton newTable;
 	private final MenuButton listOfTables;
+	private final MenuButton forward;
+	private final MenuButton previous;
 	private final GuiTempletsYaml.Builder guiTemplets;
 	Map<ItemStack, ItemStack> data = new HashMap<>();
 
 	public EditCreateLootTable() {
 		super(new ArrayList<>(LootItems.getInstance().getSettings().keySet()));
-		guiTemplets = new GuiTempletsYaml.Builder(getViewer(), "LootTables");
+		guiTemplets = new GuiTempletsYaml.Builder(getViewer(), "LootTables").placeholders(getPageNumber());
 
 		setMenuSize(guiTemplets.build().getGuiSize());
 		setTitle(guiTemplets.build().getGuiTitle());
@@ -110,6 +113,43 @@ public class EditCreateLootTable extends MenuHolder {
 				return null;
 			}
 		};
+		previous = new MenuButton() {
+			@Override
+			public void onClickInsideMenu(final Player player, final Inventory menu, final ClickType click, final ItemStack clickedItem, final Object object) {
+
+				if (click.isLeftClick()) {
+					previousPage();
+				}
+
+				UpdateTittleContainers.update(player, guiTemplets.build().getGuiTitle("LootTables", getPageNumber()), Material.CHEST, getMenu().getSize());
+				updateButtons();
+			}
+
+			@Override
+			public ItemStack getItem() {
+				GuiTempletsYaml gui = guiTemplets.menuKey("Previous_button").build();
+
+				return CreateItemUtily.of(gui.getIcon(), gui.getDisplayName(),
+						gui.getLore()).makeItemStack();
+			}
+		};
+		forward = new MenuButton() {
+			@Override
+			public void onClickInsideMenu(final Player player, final Inventory menu, final ClickType click, final ItemStack clickedItem, final Object object) {
+				if (click.isLeftClick()) {
+					nextPage();
+				}
+				UpdateTittleContainers.update(player, guiTemplets.build().getGuiTitle("LootTables", getPageNumber()), Material.CHEST, getMenu().getSize());
+				updateButtons();
+			}
+
+			@Override
+			public ItemStack getItem() {
+				GuiTempletsYaml gui = guiTemplets.menuKey("Forward_button").build();
+				return CreateItemUtily.of(gui.getIcon(), gui.getDisplayName(),
+						gui.getLore()).makeItemStack();
+			}
+		};
 	}
 
 	@Override
@@ -121,6 +161,10 @@ public class EditCreateLootTable extends MenuHolder {
 	@Override
 	public ItemStack getItemAt(int slot) {
 
+		if (guiTemplets.menuKey("Forward_button").build().getSlot().contains(slot))
+			return forward.getItem();
+		if (guiTemplets.menuKey("Previous_button").build().getSlot().contains(slot))
+			return previous.getItem();
 		if (guiTemplets.menuKey("Create_Table").build().getSlot().contains(slot))
 			return createTable.getItem();
 		if (guiTemplets.menuKey("Create_Table").build().getSlot().contains(slot))
