c517bbc7d50bc4acf5ca71aff9af1b4c2fcb1dd4
broken1arrow <broken2arrow1@gmail.com>
2022-03-22 22:56:40 +0100

    added option to add EnchantMents

--- src/main/java/org/brokenarrow/lootboxes/menus/CustomizeItem.java
+++ src/main/java/org/brokenarrow/lootboxes/menus/CustomizeItem.java
@@ -3,7 +3,10 @@ package org.brokenarrow.lootboxes.menus;
 import org.brokenarrow.lootboxes.Lootboxes;
 import org.brokenarrow.lootboxes.builder.GuiTempletsYaml;
 import org.brokenarrow.lootboxes.builder.LootData;
+import org.brokenarrow.lootboxes.commandprompt.SaveEnchantment;
+import org.brokenarrow.lootboxes.commandprompt.SeachForEnchantment;
 import org.brokenarrow.lootboxes.commandprompt.SeachForItem;
+import org.brokenarrow.lootboxes.lootdata.ItemData;
 import org.brokenarrow.lootboxes.lootdata.LootItems;
 import org.brokenarrow.lootboxes.untlity.CreateItemUtily;
 import org.brokenarrow.lootboxes.untlity.LootDataSave;
@@ -11,18 +14,23 @@ import org.brokenarrow.menu.library.MenuButton;
 import org.brokenarrow.menu.library.MenuHolder;
 import org.brokenarrow.menu.library.NMS.UpdateTittleContainers;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CustomizeItem extends MenuHolder {
 
 	private final MenuButton backButton;
 	private final MenuButton removeButton;
 	private final MenuButton changeItem;
+	private final MenuButton enchantItem;
 	private final GuiTempletsYaml.Builder guiTemplets;
 	private final LootItems lootItems = LootItems.getInstance();
 
@@ -49,7 +57,21 @@ public class CustomizeItem extends MenuHolder {
 						gui.getLore()).makeItemStack();
 			}
 		};
+		enchantItem = new MenuButton() {
+			@Override
+			public void onClickInsideMenu(Player player, Inventory inventory, ClickType clickType, ItemStack itemStack, Object o) {
+				new EnchantMents(lootTable, itemToEdit, "").menuOpen(player);
+			}
 
+			@Override
+			public ItemStack getItem() {
+				GuiTempletsYaml gui = guiTemplets.menuKey("Enchant_Item").build();
+
+				return CreateItemUtily.of(gui.getIcon(),
+						gui.getDisplayName(),
+						gui.getLore()).makeItemStack();
+			}
+		};
 		removeButton = new MenuButton() {
 
 			@Override
@@ -95,6 +117,8 @@ public class CustomizeItem extends MenuHolder {
 
 		if (guiTemplets.menuKey("Change_Item").build().getSlot().contains(slot))
 			return changeItem.getItem();
+		if (guiTemplets.menuKey("Enchant_Item").build().getSlot().contains(slot))
+			return enchantItem.getItem();
 		if (guiTemplets.menuKey("Remove_Button").build().getSlot().contains(slot))
 			return removeButton.getItem();
 		if (guiTemplets.menuKey("Back_button").build().getSlot().contains(slot))
@@ -127,7 +151,7 @@ public class CustomizeItem extends MenuHolder {
 					if (clickType.isLeftClick())
 						new SeachForItem(lootTable, itemToEdit).start(player);
 					else
-						new CustomizeItem.ChangeItem(lootTable, itemToEdit, "").menuOpen(player);
+						new ChangeItem(lootTable, itemToEdit, "").menuOpen(player);
 				}
 
 				@Override
@@ -156,7 +180,7 @@ public class CustomizeItem extends MenuHolder {
 
 				@Override
 				public ItemStack getItem(Object object) {
-					
+
 					ItemStack itemstack = null;
 					if (object instanceof Material)
 						itemstack = new ItemStack((Material) object);
@@ -236,6 +260,208 @@ public class CustomizeItem extends MenuHolder {
 
 		@Override
 		public ItemStack getItemAt(int slot) {
+
+			if (guiTemplets.menuKey("Seach_button").build().getSlot().contains(slot))
+				return seachButton.getItem();
+			if (guiTemplets.menuKey("Forward_button").build().getSlot().contains(slot))
+				return forward.getItem();
+			if (guiTemplets.menuKey("Previous_button").build().getSlot().contains(slot))
+				return previous.getItem();
+			if (guiTemplets.menuKey("Back_button").build().getSlot().contains(slot))
+				return backButton.getItem();
+
+			return null;
+		}
+	}
+
+	public static class EnchantMents extends MenuHolder {
+		private final MenuButton backButton;
+		private final MenuButton seachButton;
+		private final MenuButton forward;
+		private final MenuButton previous;
+		private final MenuButton enchantmentsList;
+		private final GuiTempletsYaml.Builder guiTemplets;
+		private final LootItems lootItems = LootItems.getInstance();
+		private final ItemData itemData = ItemData.getInstance();
+
+		public EnchantMents(String lootTable, String itemToEdit, String enchantMentsToSearchFor) {
+			super(Lootboxes.getInstance().getEnchantmentList().getEnchantments(enchantMentsToSearchFor));
+			Map<ItemStack, Enchantment> cachedEnchantment = new HashMap<>();
+			this.guiTemplets = new GuiTempletsYaml.Builder(getViewer(), "EnchantMents").placeholders(getPageNumber());
+
+			setMenuSize(guiTemplets.build().getGuiSize());
+			setTitle(guiTemplets.build().getGuiTitle());
+			setFillSpace(guiTemplets.build().getFillSpace());
+
+			seachButton = new MenuButton() {
+				@Override
+				public void onClickInsideMenu(Player player, Inventory inventory, ClickType clickType, ItemStack itemStack, Object o) {
+					//Enchantment.SILK_TOUCH
+					if (clickType.isLeftClick())
+						new SeachForEnchantment(lootTable, itemToEdit).start(player);
+					else
+						new EnchantMents(lootTable, itemToEdit, "").menuOpen(player);
+				}
+
+				@Override
+				public ItemStack getItem() {
+					GuiTempletsYaml gui = guiTemplets.menuKey("Seach_button").build();
+
+					return CreateItemUtily.of(gui.getIcon(), gui.getDisplayName(),
+							gui.getLore()).makeItemStack();
+				}
+			};
+
+			enchantmentsList = new MenuButton() {
+				@Override
+				public void onClickInsideMenu(Player player, Inventory inventory, ClickType clickType, ItemStack itemStack, Object o) {
+
+					if (o instanceof ItemStack)
+						System.out.println("object clicked " + cachedEnchantment.get(o));
+
+
+					if (clickType.isLeftClick()) {
+						if (o instanceof ItemStack)
+							new SaveEnchantment(lootTable, itemToEdit, cachedEnchantment.get(o)).start(player);
+					} else if (clickType.isRightClick()) {
+						if (o instanceof ItemStack) {
+							Enchantment enchantment = cachedEnchantment.get(o);
+							LootData data = lootItems.getLootData(lootTable, itemToEdit);
+							ItemStack item = itemData.getCacheItemData(data.getItemdataFileName(), data.getItemdataPath());
+							if (item.getItemMeta() != null) {
+								ItemMeta metadata = item.getItemMeta();
+								System.out.println("have meta before " + metadata.getEnchants());
+								System.out.println("item have meta before " + item);
+								metadata.getEnchants().remove(enchantment);
+								System.out.println("have meta after " + metadata.getEnchants());
+								System.out.println("item have meta after " + item);
+								item.setItemMeta(metadata);
+								System.out.println("have meta after setmeta " + metadata.getEnchants());
+								System.out.println("item have meta after setmeta " + item);
+							}
+							CreateItemUtily.of(item).addEnchantments(cachedEnchantment.get(o));
+
+							String filePatch = itemData.setCacheItemData(data.getItemdataPath(), item);
+
+							LootData.Builder builder = lootItems.getLootData(lootTable, itemToEdit).getBuilder();
+							builder.setHaveMetadata(true).setItemdataPath(filePatch);
+
+							lootItems.setCachedLoot(lootTable, itemToEdit, builder.build());
+						}
+					}
+			/*	if (o instanceof ItemStack) {
+						LootData data = lootItems.getLootData(lootTable, itemToEdit);
+						ItemStack item = itemData.getCacheItemData(data.getItemdataFileName(), data.getItemdataPath());
+						CreateItemUtily.of(item).addEnchantments(cachedEnchantment.get(o));
+
+						String filePatch = itemData.setCacheItemData(data.getItemdataPath(), item);
+
+						LootData.Builder builder = lootItems.getLootData(lootTable, itemToEdit).getBuilder();
+						builder.setHaveMetadata(true).setItemdataPath(filePatch);
+
+						lootItems.setCachedLoot(lootTable, itemToEdit, builder.build());
+					}
+					}else{
+
+					}*/
+
+				}
+
+				@Override
+				public ItemStack getItem() {
+					return null;
+				}
+
+				@Override
+				public ItemStack getItem(Object object) {
+
+					Enchantment enchantment = null;
+					if (object instanceof Enchantment)
+						enchantment = (Enchantment) object;
+					if (object instanceof ItemStack)
+						enchantment = cachedEnchantment.get(object);
+
+					LootData data = lootItems.getLootData(lootTable, itemToEdit);
+					ItemStack item = itemData.getCacheItemData(data.getItemdataFileName(), data.getItemdataPath());
+
+
+					GuiTempletsYaml gui = guiTemplets.menuKey("Enchant_Item").placeholders("", object instanceof Enchantment ? enchantment.getKey().getKey() : enchantment != null ? enchantment.getKey().getKey() : "").build();
+
+					//System.out.println("object " + object);
+					boolean haveEnchant = item != null && item.getItemMeta() != null && enchantment != null && item.getItemMeta().hasEnchant(enchantment);
+					if (haveEnchant)
+						gui = guiTemplets.menuKey("Item_Has_Enchantment").placeholders("", enchantment.getKey().getKey(), item.getItemMeta().getEnchants().get(enchantment).shortValue()).build();
+
+					ItemStack itemStack = CreateItemUtily.of(gui.getIcon(), gui.getDisplayName(),
+							gui.getLore()).setGlow(haveEnchant).setShowEnchantments(!haveEnchant).makeItemStack();
+					cachedEnchantment.put(itemStack, enchantment);
+					//System.out.println("itemStack " + itemStack);
+					return itemStack;
+				}
+			};
+			previous = new MenuButton() {
+				@Override
+				public void onClickInsideMenu(final Player player, final Inventory menu, final ClickType click, final ItemStack clickedItem, final Object object) {
+
+					if (click.isLeftClick()) {
+						previousPage();
+					}
+
+					UpdateTittleContainers.update(player, guiTemplets.build().getGuiTitle("EnchantMents", getPageNumber()), Material.CHEST, getMenu().getSize());
+					updateButtons();
+				}
+
+				@Override
+				public ItemStack getItem() {
+					GuiTempletsYaml gui = guiTemplets.menuKey("Previous_button").build();
+
+					return CreateItemUtily.of(gui.getIcon(), gui.getDisplayName(),
+							gui.getLore()).makeItemStack();
+				}
+			};
+			forward = new MenuButton() {
+				@Override
+				public void onClickInsideMenu(final Player player, final Inventory menu, final ClickType click, final ItemStack clickedItem, final Object object) {
+					if (click.isLeftClick()) {
+						nextPage();
+					}
+					UpdateTittleContainers.update(player, guiTemplets.build().getGuiTitle("EnchantMents", getPageNumber()), Material.CHEST, getMenu().getSize());
+					updateButtons();
+				}
+
+				@Override
+				public ItemStack getItem() {
+					GuiTempletsYaml gui = guiTemplets.menuKey("Forward_button").build();
+					return CreateItemUtily.of(gui.getIcon(), gui.getDisplayName(),
+							gui.getLore()).makeItemStack();
+				}
+			};
+			backButton = new MenuButton() {
+				@Override
+				public void onClickInsideMenu(Player player, Inventory inventory, ClickType clickType, ItemStack itemStack, Object o) {
+					new CustomizeItem(lootTable, itemToEdit).menuOpen(player);
+				}
+
+				@Override
+				public ItemStack getItem() {
+					GuiTempletsYaml gui = guiTemplets.menuKey("Back_button").build();
+
+					return CreateItemUtily.of(gui.getIcon(),
+							gui.getDisplayName(),
+							gui.getLore()).makeItemStack();
+				}
+			};
+
+		}
+
+		@Override
+		public ItemStack getFillItemsAt(Object o) {
+			return enchantmentsList.getItem(o);
+		}
+
+		@Override
+		public ItemStack getItemAt(int slot) {
+
 			if (guiTemplets.menuKey("Seach_button").build().getSlot().contains(slot))
 				return seachButton.getItem();
 			if (guiTemplets.menuKey("Forward_button").build().getSlot().contains(slot))
