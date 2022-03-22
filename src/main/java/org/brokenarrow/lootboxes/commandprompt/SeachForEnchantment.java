f019404993b99e40fe14466d31ac0cf40eeba2fe
broken1arrow <broken2arrow1@gmail.com>
2022-03-22 22:59:48 +0100

    new class for seach a enchantment

---
+++ src/main/java/org/brokenarrow/lootboxes/commandprompt/SeachForEnchantment.java
@@ -0,0 +1,38 @@
+package org.brokenarrow.lootboxes.commandprompt;
+
+import org.brokenarrow.lootboxes.menus.CustomizeItem;
+import org.bukkit.conversations.ConversationContext;
+import org.bukkit.conversations.Prompt;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class SeachForEnchantment extends SimpleConversation {
+
+	private final String lootTable;
+	private final String itemToEdit;
+
+	public SeachForEnchantment(String lootTable, String itemToEdit) {
+		this.lootTable = lootTable;
+		this.itemToEdit = itemToEdit;
+	}
+
+	@Override
+	protected Prompt getFirstPrompt() {
+		return new ItemSeach();
+	}
+
+	public class ItemSeach extends SimplePromp {
+		@Override
+		protected String getPrompt(ConversationContext context) {
+			return "Seach for item or items, type in parts or whole name of item";
+		}
+
+		@Nullable
+		@Override
+		protected Prompt acceptValidatedInput(@NotNull ConversationContext context, @NotNull String input) {
+
+			new CustomizeItem.EnchantMents(lootTable, itemToEdit, input).menuOpen(getPlayer(context));
+			return null;
+		}
+	}
+}
