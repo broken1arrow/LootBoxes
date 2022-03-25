d7d0638010a069183a7ad4b1c7cc1d62bc14dce2
broken1arrow <broken2arrow1@gmail.com>
2022-03-25 20:03:29 +0100

    added better way to handle Scheduler

---
+++ src/main/java/org/brokenarrow/lootboxes/untlity/RunTimedTask.java
@@ -0,0 +1,23 @@
+package org.brokenarrow.lootboxes.untlity;
+
+import org.brokenarrow.lootboxes.Lootboxes;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
+
+public class RunTimedTask {
+
+
+	public static BukkitTask runtask(Runnable task, boolean async) {
+		if (async)
+			return Bukkit.getScheduler().runTaskAsynchronously(Lootboxes.getInstance(), task);
+		else
+			return Bukkit.getScheduler().runTask(Lootboxes.getInstance(), task);
+	}
+
+	public static BukkitTask runtaskLater(long tick, Runnable task, boolean async) {
+		if (async)
+			return Bukkit.getScheduler().runTaskLaterAsynchronously(Lootboxes.getInstance(), task, tick);
+		else
+			return Bukkit.getScheduler().runTaskLater(Lootboxes.getInstance(), task, tick);
+	}
+}
