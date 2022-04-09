edd5759e93ce367f99ab48f48d896b7ee19bb7e8
broken1arrow <broken2arrow1@gmail.com>
2022-04-09 09:55:45 +0200

    Update LootDataSave.java
    fixed not needed ;

--- src/main/java/org/brokenarrow/lootboxes/untlity/LootDataSave.java
+++ src/main/java/org/brokenarrow/lootboxes/untlity/LootDataSave.java
@@ -1,13 +1,13 @@
 package org.brokenarrow.lootboxes.untlity;
 
 public enum LootDataSave {
-	
+
 	CHANCE,
 	MIN,
 	MAX,
 	ITEM,
 	ITEM_DATA_PATH,
 	META_DATA_FILENAME,
-	HAVE_META_DATA;
+	HAVE_META_DATA
 
 }
