378a0dd31637b2cadcea7993399f1d477d708fd4
broken1arrow <broken2arrow1@gmail.com>
2022-04-09 19:09:12 +0200

    Update PlayerClick.java
    Fixed so you can turn of click (will fix so it also open the menu again)

--- src/main/java/org/brokenarrow/lootboxes/listener/PlayerClick.java
+++ src/main/java/org/brokenarrow/lootboxes/listener/PlayerClick.java
@@ -1,5 +1,6 @@
 package org.brokenarrow.lootboxes.listener;
 
+import org.brokenarrow.lootboxes.Lootboxes;
 import org.brokenarrow.lootboxes.builder.ContainerDataBuilder;
 import org.brokenarrow.lootboxes.lootdata.ContainerData;
 import org.bukkit.Location;
@@ -9,7 +10,10 @@ import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.Map;
 
@@ -22,11 +26,16 @@ public class PlayerClick implements Listener {
 	public void playerClick(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		Block block = event.getClickedBlock();
-		if (block == null) return;
-		
-		Location location = event.getClickedBlock().getLocation();
 		if (!player.hasMetadata("addRemovecontainers")) return;
 
+		Action action = event.getAction();
+		if (action == Action.LEFT_CLICK_AIR && player.isSneaking()) {
+			player.removeMetadata("addRemovecontainers", Lootboxes.getInstance());
+			player.sendMessage("You have now turn now off add ar remove mode");
+		}
+		if (block == null) return;
+
+		Location location = block.getLocation();
 		if (block.getType() == Material.CHEST || block.getType() == Material.BARREL || block.getType() == Material.HOPPER) {
 			String metadata = (String) player.getMetadata("addRemovecontainers").get(0).value();
 			System.out.println("metadata  " + metadata);
@@ -34,23 +43,36 @@ public class PlayerClick implements Listener {
 
 			ContainerDataBuilder.Builder builder = data.getBuilder();
 			Map<Location, ContainerDataBuilder.ContainerData> containerDataMap = data.getLinkedContainerData();
-			if (!containerDataMap.containsKey(location)) {
+			if (!containerDataMap.containsKey(location) && action == Action.LEFT_CLICK_BLOCK) {
 				if (block.getBlockData() instanceof Directional) {
 					event.setCancelled(true);
 					Directional container = (Directional) block.getBlockData();
 					containerDataMap.put(location, new ContainerDataBuilder.ContainerData(container.getFacing(), block.getType()));
-					System.out.println("tesjhkvfjy playerClick facing: " + container.getFacing());
 					builder.setContainerData(containerDataMap);
 					if (data.getIcon() == null || data.getIcon() == Material.AIR)
 						builder.setIcon(block.getType());
 					containerData.setContainerData(metadata, builder.build());
-					System.out.println("tesjhkvfjy playerClick" + containerData.getCacheContainerData());
 				}
 
+			} else if (action == Action.RIGHT_CLICK_BLOCK) {
+				containerDataMap.remove(location);
+				builder.setContainerData(containerDataMap);
+				containerData.setContainerData(metadata, builder.build());
 			}
-
 		}
+	}
 
+	@EventHandler
+	public void playerLeftSever(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		if (player.hasMetadata("addRemovecontainers"))
+			player.removeMetadata("addRemovecontainers", Lootboxes.getInstance());
+	}
 
+	@EventHandler
+	public void playerLeftSever(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		if (player.hasMetadata("addRemovecontainers"))
+			player.removeMetadata("addRemovecontainers", Lootboxes.getInstance());
 	}
 }
