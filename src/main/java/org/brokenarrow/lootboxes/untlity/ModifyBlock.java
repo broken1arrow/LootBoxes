2412e3d7922ac33bc65dba29dcfd0f4d64074724
broken1arrow <broken2arrow1@gmail.com>
2022-04-09 19:03:51 +0200

    added class so I can change block data easy

---
+++ src/main/java/org/brokenarrow/lootboxes/untlity/ModifyBlock.java
@@ -0,0 +1,119 @@
+package org.brokenarrow.lootboxes.untlity;
+
+import org.bukkit.Location;
+import org.bukkit.block.*;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
+import org.bukkit.inventory.Inventory;
+
+public class ModifyBlock {
+
+
+	/**
+	 * Get the inventory on the location.
+	 *
+	 * @param location you want to get the inventory.
+	 * @return inventory or null if it not find inventory.
+	 */
+	public static void setCustomName(Location location) {
+		Block block = location.getBlock();
+		if (!checkBlocktype(block)) return;
+		String text = "test if works too end";
+
+		switch (block.getType()) {
+			case HOPPER:
+				Hopper hopper = ((Hopper) block.getState());
+				hopper.setCustomName(text);
+				hopper.update();
+				break;
+			case DISPENSER:
+				Dispenser dispenser = ((Dispenser) block.getState());
+				dispenser.setCustomName(text);
+				dispenser.update();
+				break;
+			case DROPPER:
+				Dropper dropper = ((Dropper) block.getState());
+				dropper.setCustomName(text);
+				dropper.update();
+				break;
+			case BARREL:
+				Barrel barrel = ((Barrel) block.getState());
+				barrel.setCustomName(text);
+				barrel.update();
+				break;
+			case CHEST:
+			case TRAPPED_CHEST:
+				Chest chest = ((Chest) block.getState());
+				chest.setCustomName(text);
+				chest.update();
+				break;
+			default:
+				break;
+		}
+	}
+
+	public static void setRotation(Location location, BlockFace latitude) {
+		Block block = location.getBlock();
+		BlockData blockData = block.getBlockData();
+		if (blockData instanceof Directional) {
+			((Directional) blockData).setFacing(latitude);
+			block.setBlockData(blockData);
+		}
+	}
+
+
+	/**
+	 * Get the inventory on the location.
+	 *
+	 * @param location you want to get the inventory.
+	 * @return inventory or null if it not find inventory.
+	 */
+	public static Inventory getInventory(Location location) {
+		Block block = location.getBlock();
+		if (!checkBlocktype(block)) return null;
+
+		Inventory inventory = null;
+		switch (block.getType()) {
+			case HOPPER:
+				inventory = ((Hopper) block.getState()).getInventory();
+				break;
+			case DISPENSER:
+				inventory = ((Dispenser) block.getState()).getInventory();
+				break;
+			case DROPPER:
+				inventory = ((Dropper) block.getState()).getInventory();
+				break;
+			case BARREL:
+				inventory = ((Barrel) block.getState()).getInventory();
+				break;
+			case CHEST:
+			case TRAPPED_CHEST:
+				inventory = ((Chest) block.getState()).getInventory();
+				break;
+			default:
+				break;
+		}
+		return inventory;
+	}
+
+	/**
+	 * Check if it valid container some can store items.
+	 *
+	 * @param block block you want to check
+	 * @return true if it container.
+	 */
+	public static boolean checkBlocktype(Block block) {
+		switch (block.getType()) {
+			case HOPPER:
+			case DISPENSER:
+			case DROPPER:
+			case BARREL:
+			case CHEST:
+			case TRAPPED_CHEST:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+}
