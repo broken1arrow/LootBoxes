cec07831688721676928460afc0fe1358f1883a2
broken1arrow <broken2arrow1@gmail.com>
2022-04-21 18:19:41 +0200

    Update Settings.java
    added new settings.

--- src/main/java/org/brokenarrow/lootboxes/settings/Settings.java
+++ src/main/java/org/brokenarrow/lootboxes/settings/Settings.java
@@ -32,13 +32,19 @@ public class Settings extends YamlUtil {
 		boolean warnBeforeSaveWithMetadata = customConfig.getBoolean("Warn_Before_Save_With_Metadata");
 		boolean randomContinerSpawn = customConfig.getBoolean("Random_Continer_Spawn");
 		boolean saveMetadataOnItem = customConfig.getBoolean("Save_Metadata_On_Item");
-
+		int increse = customConfig.getInt("Max_and_min_amount.Increse_with");
+		int decrese = customConfig.getInt("Max_and_min_amount.Decrese_with");
+		String language = customConfig.getString("Language");
 		SettingsData settingsData = new SettingsData.Builder()
 				.setAmountOfBlocksBelowSurface(amountOfBlocksBelowSurface)
 				.setWarnBeforeSaveWithMetadata(warnBeforeSaveWithMetadata)
 				.setSaveMetadataOnItem(saveMetadataOnItem)
 				.setSpawnOnSurface(spawnOnSurface)
-				.setRandomContinerSpawn(randomContinerSpawn).build();
+				.setRandomContinerSpawn(randomContinerSpawn)
+				.setIncrese(increse)
+				.setDecrese(decrese)
+				.setLanguage(language)
+				.build();
 
 		settings.put("Settings", settingsData);
 	}
