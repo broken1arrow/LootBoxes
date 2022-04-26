daec0a25cc0ccc2177cf09905ac737fcb552c550
broken1arrow <broken2arrow1@gmail.com>
2022-04-26 20:02:49 +0200

    Create EntityKeyData.java
    class for help with store keys name and ContainerData name.

---
+++ src/main/java/org/brokenarrow/lootboxes/builder/EntityKeyData.java
@@ -0,0 +1,19 @@
+package org.brokenarrow.lootboxes.builder;
+
+public class EntityKeyData {
+	private final String keyName;
+	private final String containerDataFileName;
+
+	public EntityKeyData(String keyName, String containerDataFileName) {
+		this.keyName = keyName;
+		this.containerDataFileName = containerDataFileName;
+	}
+
+	public String getKeyName() {
+		return keyName;
+	}
+
+	public String getContainerDataFileName() {
+		return containerDataFileName;
+	}
+}
