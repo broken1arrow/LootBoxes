4a3b5a4644ba218d8e7b4218a5c7edd5ef54368a
broken1arrow <broken2arrow1@gmail.com>
2022-04-26 20:07:41 +0200

    Create MobDropListener.java
    Not done yet, but class to Listen if mob get killed and drop right key.

---
+++ src/main/java/org/brokenarrow/lootboxes/listener/MobDropListener.java
@@ -0,0 +1,44 @@
+package org.brokenarrow.lootboxes.listener;
+
+import org.brokenarrow.lootboxes.builder.ContainerDataBuilder;
+import org.brokenarrow.lootboxes.lootdata.ContainerData;
+import org.brokenarrow.lootboxes.untlity.CreateItemUtily;
+import org.brokenarrow.lootboxes.untlity.RandomKey;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class MobDropListener implements Listener {
+	ContainerData containerData = ContainerData.getInstance();
+
+	@EventHandler
+	public void playerLogIn(PlayerJoinEvent event) {
+		Map<String, ContainerDataBuilder.KeysData> keysData = containerData.getCacheKeysData("Global_Container");
+
+		for (Map.Entry<String, ContainerDataBuilder.KeysData> data : keysData.entrySet()) {
+			event.getPlayer().getInventory().addItem(CreateItemUtily.of(data.getValue().getItemType(), data.getValue().getDisplayName(), data.getValue().getLore()).setItemMetaData("test", data.getKey()).makeItemStack());
+		}
+	}
+
+	@EventHandler
+	public void MobDrops(EntityDeathEvent event) {
+		final LivingEntity entity = event.getEntity();
+		if (entity.getKiller() != null && !(entity instanceof Player)) {
+			ItemStack[] itemStacks = new RandomKey().makeRandomAmountOfItems(entity.getType());
+			if (itemStacks != null)
+				for (ItemStack itemStack : itemStacks)
+					entity.getLocation().getWorld().dropItemNaturally(entity.getLocation(), itemStack);
+			System.out.println("kilded entety you get " + Arrays.toString(itemStacks));
+			System.out.println("kilded entety " + entity);
+		}
+	}
+
+
+}
