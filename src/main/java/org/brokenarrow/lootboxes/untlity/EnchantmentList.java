985b1be7e51e78977fd1f7c33b27f93611f496f3
broken1arrow <broken2arrow1@gmail.com>
2022-04-26 20:25:18 +0200

    Update EnchantmentList.java
    temporary "fix" some older minecraft versions do not have getKey().

--- src/main/java/org/brokenarrow/lootboxes/untlity/EnchantmentList.java
+++ src/main/java/org/brokenarrow/lootboxes/untlity/EnchantmentList.java
@@ -2,6 +2,7 @@ package org.brokenarrow.lootboxes.untlity;
 
 import org.bukkit.enchantments.Enchantment;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,11 +10,15 @@ import java.util.stream.Stream;
 
 public class EnchantmentList {
 
-	private final List<Enchantment> enchantments;
+	private List<Enchantment> enchantments = new ArrayList<>();
 
 	public EnchantmentList() {
-		this.enchantments = Stream.of(Enchantment.values())
-				.sorted(Comparator.comparing(o -> o.getKey().getKey())).collect(Collectors.toList());
+		try {
+			this.enchantments = Stream.of(Enchantment.values())
+					.sorted(Comparator.comparing(o -> o.getKey().getKey())).collect(Collectors.toList());
+		} catch (NoSuchMethodError ignored) {
+		}
+
 	}
 
 	public List<Enchantment> getEnchantments(String enchantMentsToSearchFor) {
