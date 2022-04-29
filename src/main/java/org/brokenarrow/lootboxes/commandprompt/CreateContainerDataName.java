3f16e3fa9163da0f5dd796d01259d94bd12b0b39
broken1arrow <broken2arrow1@gmail.com>
2022-04-29 10:16:50 +0200

    Update CreateContainerDataName.java
    change method name.

--- src/main/java/org/brokenarrow/lootboxes/commandprompt/CreateContainerDataName.java
+++ src/main/java/org/brokenarrow/lootboxes/commandprompt/CreateContainerDataName.java
@@ -41,7 +41,7 @@ public class CreateContainerDataName extends SimpleConversation {
 				return getFirstPrompt();
 			}
 			CREATE_CONTAINER_DATA_NAME_CONFIRM.sendMessage(getPlayer(context), input);
-			containerDataCache.putCacheContainerData(input, material);
+			containerDataCache.setCacheContainerDataCache(input, material);
 			new ModifyContinerData().menuOpen(getPlayer(context));
 			return null;
 		}
