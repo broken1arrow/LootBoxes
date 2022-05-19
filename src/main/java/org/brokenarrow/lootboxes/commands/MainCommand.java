51b50bdcf90a2c1fbb50eb29598a54ad5405d298
broken1arrow <broken2arrow1@gmail.com>
2022-05-19 16:14:40 +0200

    Update MainCommand.java
    not in use, only for test.

--- src/main/java/org/brokenarrow/lootboxes/commands/MainCommand.java
+++ src/main/java/org/brokenarrow/lootboxes/commands/MainCommand.java
@@ -9,21 +9,19 @@ public class MainCommand extends SubCommandsUtility {
 
 	public MainCommand() {
 		super("loot");
-		setPermission("not.in.use");
+
 	}
 
 	@Override
 	protected void onCommand() {
-		new CreateTable().start(getPlayer());
+		if (hasPerm("not.in.use"))
+			new CreateTable().start(getPlayer());
 	}
 
 	@Override
 	protected List<String> tabComplete() {
-		System.out.println("labels deddddd " + getLabel());
 		if (getArgs().length == 1)
-
 			return completeLastWord("rbg");
-
 		return null;
 	}
 }
