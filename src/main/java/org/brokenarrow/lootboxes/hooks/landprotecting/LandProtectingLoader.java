3e6ca499824350e33e24ffb237dd5072bb4caf90
broken1arrow <broken2arrow1@gmail.com>
2024-11-22 21:40:44 +0100

    Make the plugin work with WorldGuard on legacy
    Fix so the plugin works when using legacy WorldGuard and old server versions.

--- src/main/java/org/brokenarrow/lootboxes/hooks/landprotecting/LandProtectingLoader.java
+++ src/main/java/org/brokenarrow/lootboxes/hooks/landprotecting/LandProtectingLoader.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.Plugin;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
 
 public class LandProtectingLoader {
 	private final ProtectingProvider[] protectingProvider;
@@ -13,12 +14,18 @@ public class LandProtectingLoader {
 	public LandProtectingLoader(Plugin plugin) {
 		Set<ProtectingProvider> providers = new HashSet<>();
 		if (Bukkit.getServer().getPluginManager().getPlugin("WorldGuard") != null) {
+			try {
+				Class.forName("com.sk89q.worldguard");
+			} catch (ClassNotFoundException exception){
+				plugin.getLogger().log(Level.INFO,"Can't find WorldGuard, probably you using legacy WorldGuard");
+				this.protectingProvider = null;
+				return;
+			}
 			providers.add(new WorldguardProtection());
 		}
 		if (Bukkit.getServer().getPluginManager().getPlugin("Lands") != null) {
 			providers.add(new LandsProtection(plugin));
 		}
-
 		this.protectingProvider = providers.toArray(new ProtectingProvider[0]);
 	}
 
